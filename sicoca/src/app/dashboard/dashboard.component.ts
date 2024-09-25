1dab2c352a9e20f86c506d921ec1f9292969cec4
GARY\eduar <eduargabor93@gmail.com>
2024-09-24 21:31:53 -0600

    restarting

--- sicoca/src/app/dashboard/dashboard.component.ts
+++ sicoca/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { DashboardMenuComponent } from "./dashboard-menu/dashboard-menu.component";
-import { TestFirebaseDbComponent } from "../test-firebase-db/test-firebase-db.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DashboardMenuComponent, TestFirebaseDbComponent],
+  imports: [DashboardMenuComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'] // Corrige 'styleUrl' a 'styleUrls'
 })
